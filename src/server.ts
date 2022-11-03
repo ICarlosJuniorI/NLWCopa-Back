664f80dbeb9ff1918ce78b6819b7b9a382ffd45b
Carlos Junior <carlos-junior08@hotmail.com>
2022-11-03 17:50:39 -0300

    Aula 01

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -15,10 +17,46 @@ async function bootstrap() {
     origin: true, //Permite qualquer aplicação acessar o back-end
   })
 
+  //Retorna a listagem de bolões
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  //Retorna a listagem de usuários
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  //Retorna a listagem de palpites
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  //Cria um novo bolão
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+    
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      }
+    })
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
